650fa9e7a1a7fb1d1305c3a005bceef08bca729d
Otfried Cheong <ipe@otfried.org>
2020-07-20 14:55:39 +0200

    Change distances to integers

--- exponential.py
+++ exponential.py
@@ -1,13 +1,13 @@
 #
-# Count number of linear pieces for different distributions
+# Count number of linear pieces for an exponential example
 #
 
 import sys
 import time
 
-import explicit
-
 from fractions import Fraction
+
+import explicit
 
 assert sys.hexversion >= 0x3000000
 
@@ -24,8 +24,12 @@ def count_pieces(d):
 # --------------------------------------------------------------------
 
 if __name__ == "__main__":
-  d = [ None, Fraction(11569, 65536), Fraction(1537, 2048), Fraction(1), Fraction(3, 2),
-        Fraction(213, 128), Fraction(71, 32), Fraction(143, 64), Fraction(9, 4), Fraction(1, 2) ]
-  p = count_pieces(d)
+  #a = 2 * 65536 + 16536
+  #b = 65536 + 32768 + 8192 + 2048 + 512
+  #d = [ None, 11569, 49184, 65536, 98304, 109056, 145408, 146432, 147456, 32768 ]
+  #d = [ None, 11569, 32768 + 16384 + 32, 65536, 65536 + 32768, b, a - 2048, a - 1024, a, 32768 ]
+  d = [ None, 0x2d31, 0xc020, 0x10000, 0x18000, 0x1aa00, 0x24000 - 0x800, 0x24000 - 0x400, 0x24000, 0x8000 ]
+  d = list(map(lambda x: Fraction(x) if x is not None else None, d))
+  count_pieces(d)
 
 # --------------------------------------------------------------------
